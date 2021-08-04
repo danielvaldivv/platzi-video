e20b21709df05f17ed325353ba7464767b12d49c
danielvaldivv <dvaldiviesomerino.drvm@gmail.com>
2021-08-04 08:56:32 -0500

    get and set implementation

--- assets/dist/MediaPlayer.js
+++ assets/dist/MediaPlayer.js
@@ -1,21 +1,59 @@
 function MediaPlayer(config) {
     this.media = config.element;
+    this.plugins = config.plugins || [];
+
+    this._initPlugins();
 }
+
+MediaPlayer.prototype._initPlugins = function(){
+    const player = {
+        play: () => this.play(),
+        pause: () => this.pause(),
+        media: this.media,
+        get muted() {
+            return this.media.muted;
+        },
+        set muted(value) {
+            this.media.muted = value;
+        }
+    };
+
+    this.plugins.forEach(plugin => {
+        plugin.run(player);
+    });
+};
 
 MediaPlayer.prototype.toggleRunStop = function() {
     if (this.media.paused) {
         this.media.play()
     } else {
         this.media.pause()
+
     }
 }
 
-MediaPlayer.prototype.run = function() {
+MediaPlayer.prototype.toggleMuteUnmuted = function() {
+    if (this.media.muted ) {
+        this.media.muted = false;
+        this.media.unmuted = true;
+    } else  {
+        this.media.muted = true;
+        // this.media.unmuted = false;
+    }
+}
+
+MediaPlayer.prototype.play = function() {
     this.media.play();
 };
 
 MediaPlayer.prototype.stop = function() {
     this.media.pause();
+}
+MediaPlayer.prototype.mute = function() {
+    this.media.muted = true;
+}
+MediaPlayer.prototype.unmute = function() {
+    this.media.unmuted = false;
 }
 
 export default MediaPlayer;
--- assets/dist/index.js
+++ assets/dist/index.js
@@ -1,7 +1,12 @@
 import MediaPlayer from './MediaPlayer.js';
+import AutoPlay from '../plugins/AutoPlay.js'
 
 const video = document.querySelector('video');
-const button = document.querySelector('button');
-const player = new MediaPlayer({ element: video});
-button.onclick = () =>  player.toggleRunStop();
+const buttonPlay = document.querySelector('#play-stop');
+const buttonSound = document.querySelector('#mute-unmuted')
+const player = new MediaPlayer({ element: video, plugins: [
+    new AutoPlay()
+]});
+buttonPlay.onclick = () =>  player.toggleRunStop();
 video.onclick = () =>  player.toggleRunStop();
+buttonSound.onclick = () => player.toggleMuteUnmuted();
