c82e475ccc6bb2958d33bf3442e03e077aed71a3
danielvaldivv <dvaldiviesomerino.drvm@gmail.com>
2021-08-24 14:10:20 -0500

    Platzi Video React modules

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
-import React from "react";
-import  ReactDOM  from "react-dom";
-import { HelloWorld } from "./components/HelloWorld"
-
+import React from 'react';
+import ReactDOM from 'react-dom';
+import HelloWorld from "./components/HelloWorld"
 
 ReactDOM.render(
   <HelloWorld />,
-  document.getElementById(app)
+  document.getElementById('app')
 )
