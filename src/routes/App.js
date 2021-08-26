3df9bc4ce5efe8a4a7314fca40c6dfd82828ea30
danielvaldivv <dvaldiviesomerino.drvm@gmail.com>
2021-08-26 18:16:44 -0500

    bug

--- src/routes/App.js
+++ src/routes/App.js
@@ -7,23 +7,14 @@ import { createBrowserHistory } from 'history';
 // const history = createBrowserHistory();
 
 const App = () => (
-//   <BrowserRouter>
-//     <Switch>
-//       <Route exact path="/" component={Home} />
-//       <Route exact path="/login" component={Login} />
-//     </Switch>
-// </BrowserRouter>
+  <BrowserRouter>
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/login" component={Login} />
+    </Switch>
+</BrowserRouter>
 
-<Router>
-  <Switch>
-    <Route path="/" >
-      <Home />
-    </Route>
-    <Route path="/login">
-      <Login />
-    </Route>
-  </Switch>
-</Router>
+
 );
 
 export default App;
