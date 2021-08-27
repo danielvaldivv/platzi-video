fe4205b55c27abd2a897576b7a5084a9ee167521
danielvaldivv <dvaldiviesomerino.drvm@gmail.com>
2021-08-26 20:21:11 -0500

    bug

--- src/routes/App.js
+++ src/routes/App.js
@@ -9,7 +9,7 @@ const App = () => (
 <Router>
   <Switch>
     <Route exact path="/" component={Home} />
-    <Route exact path={useHistory.push("/login")} component={Login} />
+    <Route exact path="/login" component={Login} />
   </Switch>
 </Router>
 )
