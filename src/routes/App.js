4eafc12831ad927305945562d0101ab445e28421
danielvaldivv <dvaldiviesomerino.drvm@gmail.com>
2021-08-26 20:19:55 -0500

    bug

--- src/routes/App.js
+++ src/routes/App.js
@@ -1,17 +1,15 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
-import { createBrowserHistory } from 'history';
+import { BrowserRouter as Router, Route, Switch, useHistory} from 'react-router-dom';
 
 import Home from '../containers/Home';
 import Login from '../containers/Login';
 
-const history = createBrowserHistory();
 
 const App = () => (
-<Router history={history}>
+<Router>
   <Switch>
     <Route exact path="/" component={Home} />
-    <Route exact path="/login" component={Login} />
+    <Route exact path={useHistory.push("/login")} component={Login} />
   </Switch>
 </Router>
 )
