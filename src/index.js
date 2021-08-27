0088a8bb0191e5f5ff957e52d8d961b81b92482f
danielvaldivv <dvaldiviesomerino.drvm@gmail.com>
2021-08-27 18:26:10 -0500

    Debuggin with redux Devtools bug

--- src/index.js
+++ src/index.js
@@ -172,7 +172,7 @@ const initialState = {
 }
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-const store = createStore(reducer, initialState, composeEnhancers())
+const store = createStore(reducer, initialState, composeEnhancers)
 
 ReactDOM.render(
   <Provider store = {store}>
