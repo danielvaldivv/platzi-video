f67205a5bc4f2bad5aca1e6f22182c1d913b6d6b
danielvaldivv <dvaldiviesomerino.drvm@gmail.com>
2021-08-27 11:57:25 -0500

    Solving /link(typo), player import styles

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
-    pubicPath: '/',
+    publicPath: '/',
   },
   resolve: {
     extensions: ['.js', '.jsx'],
