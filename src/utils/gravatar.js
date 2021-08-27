bb3c200f10917ea01de887e29a8e7f209951c386
danielvaldivv <dvaldiviesomerino.drvm@gmail.com>
2021-08-27 10:31:43 -0500

    Using gravatar into the proyect

--- src/utils/gravatar.js
+++ src/utils/gravatar.js
@@ -2,6 +2,9 @@ import md5 from "md5";
 
 const gravatar = email => {
   const base = 'https://gravatar.com/avatar/';
-  const formateEmail = (email).trim().toLowerCase();
-  const hash = md5(formateEmail, {encoding: "binary"})
-}
+  const formattedEmail = (email).trim().toLowerCase();
+  const hash = md5(formattedEmail, {encoding: "binary"})
+  return `${base}${hash}`
+};
+
+export default gravatar
