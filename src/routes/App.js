07e7ab0a5dbce6ccd327998f71864d1222748d4b
danielvaldivv <dvaldiviesomerino.drvm@gmail.com>
2021-08-26 22:13:53 -0500

    Layout Component of header and Footer

--- src/routes/App.js
+++ src/routes/App.js
@@ -15,7 +15,7 @@ const App = () => (
     <Switch>
       <Route exact path="/" component={Home} />
       <Route exact path="/login" component={Login} />
-      <Route exact path ="/register" component={Register} /
+      <Route exact path ="/register" component={Register} />
       <Route component = {NotFound} />
     </Switch>
   </Layout>
