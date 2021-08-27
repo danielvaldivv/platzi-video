ca0ca049049db2ac2e144ae10e80e01f7055865b
danielvaldivv <dvaldiviesomerino.drvm@gmail.com>
2021-08-27 10:18:13 -0500

    creating function of gravatar using md5 library

---
+++ src/utils/gravatar.js
@@ -0,0 +1,7 @@
+import md5 from "md5";
+
+const gravatar = email => {
+  const base = 'https://gravatar.com/avatar/';
+  const formateEmail = (email).trim().toLowerCase();
+  const hash = md5(formateEmail, {encoding: "binary"})
+}
