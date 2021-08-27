a255690789766a1dcdf17cb44c289fe44a563d39
danielvaldivv <dvaldiviesomerino.drvm@gmail.com>
2021-08-26 19:22:54 -0500

    bug

--- src/routes/App.js
+++ src/routes/App.js
@@ -5,22 +5,18 @@ import Home from '../containers/Home';
 import Login from '../containers/Login';
 
 
-const App = () => {
-return (
+const App = () => (
 <Router>
-  <div>
-    <Switch>
-      <Route exact path="/">
-        <Home />
-      </Route>
-
-        <Route path="/login">
-          <Login />
-        </Route>
-    </Switch>
-  </div>
+  <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+  </Switch>
 </Router>
 )
-};
+;
 
 export default App;
