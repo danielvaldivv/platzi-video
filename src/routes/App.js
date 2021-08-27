0d2ebda025948f8d219c4e8ef0e3f164a2b99272
danielvaldivv <dvaldiviesomerino.drvm@gmail.com>
2021-08-26 19:42:13 -0500

    bug

--- src/routes/App.js
+++ src/routes/App.js
@@ -1,17 +1,17 @@
 import React from 'react';
-import { BrowserRouter , Route, Switch} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
 import Home from '../containers/Home';
 import Login from '../containers/Login';
 
 
 const App = () => (
-<BrowserRouter>
-  {/* <Switch> */}
+<Router>
+  <Switch>
     <Route exact path="/" component={Home} />
     <Route exact path="/login" component={Login} />
-  {/* </Switch> */}
-</BrowserRouter>
+  </Switch>
+</Router>
 )
 ;
 
