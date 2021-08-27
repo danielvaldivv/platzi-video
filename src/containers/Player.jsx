800986a014fa97aa12e841b90d62e9a3aea576fe
danielvaldivv <dvaldiviesomerino.drvm@gmail.com>
2021-08-27 16:05:47 -0500

    bug

--- src/containers/Player.jsx
+++ src/containers/Player.jsx
@@ -9,28 +9,28 @@ const Player = props => {
   const hasPlaying = Object.keys(props.playing).length > 0;
 
   useEffect(() => {
-    props.getVideoSource(id)
-  }, [])
+    props.getVideoSource(id);
+  }, []);
 
-  return hasPlaying
-  ? (
-      <div className="Player">
-        <video controls autoPlay>
-          <source src={props.playing.source} type="video/mp4" />
-        </video>
-        <div className="Player-back">
-          <button type="button" onClick={() => props.history.goBack()}>
-            Regresar
-          </button>
+  return (hasPlaying
+    ? ( <div className="Player">
+          <video controls autoPlay>
+            <source src={props.playing.source} type="video/mp4" />
+          </video>
+          <div className="Player-back">
+            <button type="button" onClick={() => props.history.goBack()}>
+              Regresar
+            </button>
+          </div>
         </div>
-      </div>
-    )
-  : <NotFound/>;
+      )
+    : <NotFound/>
+  )
 };
 
 const mapStateToProps = state => {
   return{
-    playing: state.playing
+    playing: state.playing,
   }
 }
 
@@ -38,4 +38,4 @@ const mapDispatchToProps = {
   getVideoSource,
 }
 
-export default connect(mapStateToProps, mapDispatchToProps) (Player)
+export default connect(mapStateToProps, mapDispatchToProps)(Player);
